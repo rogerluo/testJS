ddb99002c77370e065c57c884c81ea9ddb5fb8af
rogerluo <luodingjia1985@163.com>
2013-12-16 17:48:15 +0800

    delete

--- checkit/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpwd 	= "intel@123";
-	$dbinst = "checkit";
-?>
--- checkit/test.py
+++
@@ -1,7 +0,0 @@
-import re
-line = "		#1036	GV6 TEXT	= BID_HI	(TRWF_MF_RMTES_STRING - Length=6)"
-#line = line.strip()
-print line
-r = re.compile('^\t\t#(\d{1,5})[^=]+=\s([^\(]+)\(.*')
-m = r.match(line)
-print m.groups(0)[2]
